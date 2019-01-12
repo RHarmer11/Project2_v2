42ca985386982816a08afcf1a703010a85580a56
RHarmer11 <42324086+RHarmer11@users.noreply.github.com>
2019-01-12 13:01:14 -0500

    Newer js file

--- static/js/app.js
+++ static/js/app.js
@@ -1,66 +1,22 @@
-function buildCharts(Stocks) {
-  
-  // @TODO: Use `d3.json` to fetch the sample data for the plots
-  // var url = `/samples/${sample}`;
-  var url =`/data`;
-  d3.json(url).then(function(data) {
+var dataPoints =[];
 
-    // @TODO: Build a Bubble Chart using the sample data
-    // var x_values = data.otu_ids;
-    // var y_values = data.sample_values;
-    // var marker_size = data.sample_values;
-    // var marker_colors = data.otu_ids; 
-    // var text_values = data.otu_labels;
+function getDataPoints(csv) {
+    var dataPoints =csvLines =points =[];
+    csvLines = csv.split(/[\r?\n|\r|\n]+/);
 
-    var trace1 = {
-      x: x_values,
-      y: y_values,
-      text: text_values,
-      mode: 'markers',
-      marker: {
-        color: marker_colors,
-        size: marker_size
-      } 
+    for(var i=0; i < csvLines.length; i++)
+        if(csvLines[i].length > 0) {
+            points = csvLines[i].split(",");
+            dataPoints.push({
+                x:parseFloat(points[0]),
+                y:parseFloat(points[1])
+            });
+        }
+    return dataPoints;
+}
+$.get("https://github.com/RHarmer11/Project2_v2/blob/master/result2.csv",function(data) {
+    var chart = new CanvasJS.Chart('chartContainer'), {
+       
     };
-  
-    var data = [trace1];
-
-    var layout = {
-      // xaxis: { title: "OTU ID"},
-    };
-
-    Plotly.newPlot('bubble', data, layout);
-
-
-// then the data response
-// build some plotly plots
-function init() {
-    // Grab a reference to the dropdown select element
-    var selector = d3.select("#selDataset");
-  
-    // Use the list of sample names to populate the select options
-    d3.json("/sectors").then((sectorNames) => {
-      sectorNames.forEach((Sector) => {
-        selector
-          .append("option")
-          .text(Sector)
-          .property("value", Sector);
-      });
-  
-      // Use the first sample from the list to build the initial plots
-      const firstSector = sectorNames[0];
-      buildCharts(firstSector);
-      buildMetadata(firstSector);
-    });
-  }
-  
-  function optionChanged(newSector) {
-    // Fetch new data each time a new sample is selected
-    buildCharts(newSector);
-    buildMetadata(newSector);
-  }
-  
-  // Initialize the dashboard
-  init();
-
-  console.log("HEY I AM RUNNING THIS IS APP DOT JS")
+    chart.render();
+})
